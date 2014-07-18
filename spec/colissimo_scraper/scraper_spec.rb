1820d9aa3a93344d6be353c8fd558756cb982b43
Raimondas Valickas <raimondas@vinted.com>
2014-07-18 13:39:31 +0300

    Adds additional mocking tests

--- spec/colissimo_scraper/scraper_spec.rb
+++ spec/colissimo_scraper/scraper_spec.rb
@@ -4,6 +4,7 @@ describe ColissimoScraper do
 
   describe ".get_tracking_list" do
 
+    let(:bytes) { File.binread(File.dirname(__FILE__) + '/assets/' + file) }
     let(:tracker) { ColissimoScraper.get_tracking_list(parcel_number) }
 
     context "call actual service" do
@@ -59,55 +60,68 @@ describe ColissimoScraper do
 
     context "mocked states" do
       let(:parcel_number) { '8J00111111111'}
-      let(:image_bytes) { File.binread(File.dirname(__FILE__) + '/assets/' + image_file) }
 
       before do
         expect(ColissimoScraper).to receive(:get_page_response).with('8J00111111111').and_return('fake_content')
         expect_any_instance_of(ColissimoScraper::Response).to receive(:each_image_url).and_yield(
             'fake_url', ColissimoScraper::DESCR_FIELD_URL, 1)
-        expect_any_instance_of(ColissimoScraper::ImageHashTracker).to receive(:get_image).and_return(image_bytes)
+        expect_any_instance_of(ColissimoScraper::ImageHashTracker).to receive(:get_image).and_return(bytes)
       end
 
       context "mock unrecognized state" do
-        let(:image_bytes) { 'unrecognized_bytes '}
+        let(:bytes) { 'unrecognized_bytes '}
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::UNRECOGNISED) }
       end
 
       context "mock 'Your parcel was delivered to the caretaker or frontdesk' state" do
-        let(:image_file) { 'frontdesk.png'  }
+        let(:file) { 'frontdesk.png'  }
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::DELIVERED) }
       end
 
       context "mock 'Your parcel is ready for delivery' state" do
-        let(:image_file) { 'delivery.png'  }
+        let(:file) { 'delivery.png'  }
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::ON_DELIVERY) }
       end
 
       context "mock 'Your parcel has arrived at its delivery location' state" do
-        let(:image_file) { 'location.png'  }
+        let(:file) { 'location.png'  }
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::IN_TRANSIT) }
       end
 
       context "mock 'La Poste is handling your parcel. It is currently being routed.' state" do
-        let(:image_file) { 'routed.png'  }
+        let(:file) { 'routed.png'  }
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::IN_TRANSIT) }
       end
 
       context "mock 'You parcel has been dropped-off at the shipping post office' state" do
-        let(:image_file) { 'dropped.png'  }
+        let(:file) { 'dropped.png'  }
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::IN_TRANSIT) }
       end
 
       context "mock 'Your parcel is ready to be shipped. It has not been taken on by La Poste yet.' state" do
-        let(:image_file) { 'shipped.png'  }
+        let(:file) { 'shipped.png'  }
         subject(:track_list) { tracker.tracking_list }
         it { expect { |b| track_list.map(&:status).each(&b) }.to yield_successive_args(ColissimoScraper::Status::IN_TRANSIT) }
+      end
+    end
+
+    context "mock parent page responses" do
+
+      let(:file) { 'delivered_request.html' }
+      let(:resp) { ColissimoScraper::Response.new(bytes) }
+      
+      context "and expect image url" do
+        let(:url_array) { 
+          (Enumerator.new { |enum| resp.each_image_url { |url, type, index| enum << url } }).to_a
+        }
+
+        it { expect(url_array.size).to be(21) }
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,3 @@ require 'colissimo_scraper/colissimo_scraper'
 require 'colissimo_scraper/response'
 require 'colissimo_scraper/status'
 require 'colissimo_scraper/image_hash_tracker'
-
